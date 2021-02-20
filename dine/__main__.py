69900b4e65d5b7252bf65269c19c0b0ea621c09e
YudaiSaito <yudai.saito.dev@gmail.com>
2021-02-20 23:41:59 +0900

    Add: discordbot起動確認

--- dine/__main__.py
+++ dine/__main__.py
@@ -1,7 +1,10 @@
 #coding : utf-8
 
+from discordbot import Dine
+
 def main():
-    pass
+    bot = Dine()
+    bot.begin()
 
 if __name__ == "__main__":
     main()
--- dine/discordbot.py
+++ dine/discordbot.py
@@ -1,1 +1,15 @@
 #coding : utf-8
+
+import os
+import discord
+from discord.ext import commands
+
+class Dine(commands.Bot):
+    def __init__(self):
+        super().__init__("!")
+    
+    async def on_ready(self):
+        await self.change_presence(status=discord.Status.idle, activity=discord.Game("dine!"))
+
+    def begin(self):
+        self.run(os.environ["DISCORD_TOKEN"])
