2002bf4ef68127520cae57d626cc99108cfec7b3
YudaiSaito <yudai.saito.dev@gmail.com>
2021-03-27 17:18:55 +0900

    Add: FlexMessage管理用ファイル #5

---
+++ dine/flex_message.py
@@ -0,0 +1,18 @@
+follow_flex_message = {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "setting password",
+          "weight": "bold",
+          "size": "xxl",
+          "align": "center",
+          "contents": []
+        }
+      ]
+    }
+  }
