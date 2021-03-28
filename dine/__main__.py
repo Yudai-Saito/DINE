cf897cde77cf41e6178ceb38a139d7fff05c09b8
YudaiSaito <yudai.saito.dev@gmail.com>
2021-03-28 14:29:21 +0900

    Change: discordbotをサブプロセスで起動 #10
    メインプロセスで時間制御系処理を行うため、BOTたちは他プロセスで起動させる。

--- dine/__main__.py
+++ dine/__main__.py
@@ -11,12 +11,20 @@ def line_run():
     linebot = Line()
     linebot.begin()
 
+def discord_run():
+    bot = Dine()
+    bot.begin()
+
 def main():
     line_process = Process(target=line_run, daemon=True)
     line_process.start()
 
-    bot = Dine()
-    bot.begin()
+    discord_process = Process(target=discord_run, daemon=True)
+    discord_process.start()
+
+    while True:
+        #time schedule
+        pass
 
 if __name__ == "__main__":
     logging.config.fileConfig("logging.conf")
