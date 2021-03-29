3abd2696c65e408732adb660a78df34b8cec1727
YudaiSaito <yudai.saito.dev@gmail.com>
2021-03-29 23:51:46 +0900

    Add: BOTがサーバーに参加した際にserver_idを格納 #12

--- dine/discord_bot.py
+++ dine/discord_bot.py
@@ -4,12 +4,21 @@ import os
 import discord
 from discord.ext import commands
 
+from db import DiscordCrud, SessionManager
+
 class Dine(commands.Bot):
     def __init__(self):
         super().__init__("!")
+        
+        self.discord_crud = DiscordCrud()
+        self.session_mng = SessionManager()
     
     async def on_ready(self):
         await self.change_presence(status=discord.Status.idle, activity=discord.Game("dine!"))
+
+    async def on_guild_join(self, guild):
+        with self.session_mng.session_create() as session:
+            self.discord_crud.add_join_server(session, guild.id)
 
     def begin(self):
         self.run(os.environ["DISCORD_TOKEN"])
