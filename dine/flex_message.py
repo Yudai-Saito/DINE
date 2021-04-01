18b3944933ae2a8847744e5d7aa9fb3badd9af75
YudaiSaito <yudai.saito.dev@gmail.com>
2021-04-01 14:39:01 +0900

    Add: ユーザー認証用FlexMessage #14

--- dine/flex_message.py
+++ dine/flex_message.py
@@ -16,3 +16,58 @@ follow_flex_message = {
       ]
     }
   }
+
+register_accept = {
+  "type": "bubble",
+  "direction": "ltr",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "backgroundColor": "#7FDEDBFF",
+    "contents": [
+      {
+        "type": "text",
+        "text": "server_name",
+        "weight": "bold",
+        "align": "center",
+        "contents": []
+      },
+      {
+        "type": "text",
+        "text": "にLINEを登録しますか？",
+        "weight": "bold",
+        "align": "center",
+        "contents": []
+      }
+    ]
+  },
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "postback",
+          "label": "はい",
+          "data": "register_accept"
+        },
+        "margin": "xl",
+        "height": "md",
+        "position": "relative"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "postback",
+          "label": "いいえ",
+          "data": "register_deny"
+        },
+        "margin": "xxl",
+        "height": "md",
+        "gravity": "bottom",
+        "position": "relative"
+      }
+    ]
+  }
+}
