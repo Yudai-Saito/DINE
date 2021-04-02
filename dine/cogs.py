3020ff68baf8ab41b983be7bf27c5142e0e135b9
YudaiSaito <yudai.saito.dev@gmail.com>
2021-04-02 22:16:39 +0900

    Add: Discordでパスワード入力時処理 #14

--- dine/cogs.py
+++ dine/cogs.py
@@ -19,7 +19,26 @@ class DineCog(commands.Cog):
 
     @dine.command()
     async def add(self, ctx, password):
-        pass
+        with self.session_mng.session_create() as session:
+            if self.discord_crud.exists_password(session, password) == True:
+
+                ctx.send("認証が完了しました！LINEで登録を完了させましょう！")
+                
+                with self.session_mng.session_create() as session:
+                    self.discord_crud.add_register_to_password(session, password, ctx.author.id)
+
+                with self.session_mng.session_create() as session:   
+                    line_id = self.discord_crud.register_user(session, password)
+
+                register_accept["header"]["contents"][0]["text"] = "\"{}\"".format(ctx.guild.name)
+
+                line_bot_api.push_message(line_id, [
+                                    FlexSendMessage(alt_text="登録メッセージ", contents=register_accept),
+                                    TextSendMessage("登録リンクは5分間有効となります！\n時間が過ぎてしまったら再度パスワードを発行してください！")
+                                ]
+                            )
+            else:
+                await ctx.send("入力されたパスワードは存在しません！")
 
 def setup(bot):
     return bot.add_cog(DineCog(bot))
