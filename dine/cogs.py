738230871d89671ee9e05fb5cef7240b56b1d5cb
YudaiSaito <yudai.saito.dev@gmail.com>
2021-04-02 22:14:01 +0900

    Add: cogsベース #14

--- dine/cogs.py
+++ dine/cogs.py
@@ -1,1 +1,25 @@
 #coding : utf-8
+
+from discord.ext import commands
+
+from db import DiscordCrud, SessionManager
+from line_bot import line_bot_api
+from linebot.models import TextSendMessage, FlexSendMessage
+from flex_message import register_accept
+
+class DineCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.discord_crud = DiscordCrud()
+        self.session_mng = SessionManager()
+
+    @commands.group()
+    async def dine(self, ctx):
+        pass
+
+    @dine.command()
+    async def add(self, ctx, password):
+        pass
+
+def setup(bot):
+    return bot.add_cog(DineCog(bot))
