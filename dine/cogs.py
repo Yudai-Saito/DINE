2c5f5cb9261265b4961bb9d70833811dca03e7e7
YudaiSaito <yudai.saito.dev@gmail.com>
2021-04-09 23:07:59 +0900

    Add: 既にサーバに登録している場合のメッセージ #19

--- dine/cogs.py
+++ dine/cogs.py
@@ -20,25 +20,29 @@ class DineCog(commands.Cog):
     @dine.command()
     async def add(self, ctx, password):
         with self.session_mng.session_create() as session:
-            if self.discord_crud.exists_password(session, password) == True:
-
-                ctx.send("認証が完了しました！LINEで登録を完了させましょう！")
-                
+            if self.discord_crud.exists_user(session, ctx.guild.id, ctx.author.id) == False:
                 with self.session_mng.session_create() as session:
-                    self.discord_crud.add_register_to_password(session, password, ctx.author.id, ctx.guild.id)
+                    if self.discord_crud.exists_password(session, password) == True:
 
-                with self.session_mng.session_create() as session:   
-                    line_id = self.discord_crud.register_user(session, password)
+                        await ctx.send("認証が完了しました！LINEで登録を完了させましょう！")
 
-                register_accept["header"]["contents"][0]["text"] = "\"{}\"".format(ctx.guild.name)
+                        with self.session_mng.session_create() as session:
+                            self.discord_crud.add_register_to_password(session, password, ctx.author.id, ctx.guild.id)
 
-                line_bot_api.push_message(line_id, [
-                                    FlexSendMessage(alt_text="登録メッセージ", contents=register_accept),
-                                    TextSendMessage("登録リンクは5分間有効となります！\n時間が過ぎてしまったら再度パスワードを発行してください！")
-                                ]
-                            )
+                        with self.session_mng.session_create() as session:   
+                            line_id = self.discord_crud.register_user(session, password)
+
+                        register_accept["header"]["contents"][0]["text"] = "\"{}\"".format(ctx.guild.name)
+
+                        line_bot_api.push_message(line_id, [
+                                            FlexSendMessage(alt_text="登録メッセージ", contents=register_accept),
+                                            TextSendMessage("登録リンクは5分間有効となります！\n時間が過ぎてしまったら再度パスワードを発行してください！")
+                                        ]
+                                    )
+                    else:
+                        await ctx.send("入力されたパスワードは使用済みか存在しません！")
             else:
-                await ctx.send("入力されたパスワードは使用済みか存在しません！")
+                await ctx.send("このサーバーには既に登録済みです！")
 
 def setup(bot):
     return bot.add_cog(DineCog(bot))
