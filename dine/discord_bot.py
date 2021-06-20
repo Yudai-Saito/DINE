996b4f59729906facd63e37c2c40f8378ca6f773
YudaiSaito <yudai.saito.dev@gmail.com>
2021-06-20 15:41:23 +0900

    Add: webhookがユーザにより直接削除された場合 #28
    DBからの削除とサーバーへの通知

--- dine/discord_bot.py
+++ dine/discord_bot.py
@@ -1,6 +1,7 @@
 #coding : utf-8
 
 import os
+import re
 import discord
 from discord.ext import commands
 
@@ -30,7 +31,26 @@ class Dine(commands.Bot):
             if type(channel) is discord.TextChannel:
                 await channel.send("DINEへようこそ！\nLINEメッセージを受信するチャンネルで!dine channelコマンドを実行してください！")
                 return
-            
+
+    async def on_webhooks_update(self, channel):
+        webhook_lists = await channel.guild.webhooks()
+
+        with self.session_mng.session_create() as session:
+            server_webhook = self.discord_crud.get_webhook_id(session, str(channel.guild.id))
+
+        for webhook in webhook_lists:
+            webhook = re.sub("\D", "", str(webhook.id))
+            if server_webhook[0] == webhook: 
+                return
+
+        with self.session_mng.session_create() as session:
+            server_webhook = self.discord_crud.set_webhook_id(session, str(channel.guild.id), None)
+
+        for channel in channel.guild.channels:
+            if type(channel) is discord.TextChannel:
+                await channel.send("設定されているwebhookが削除されました！\nLINEを受信するには !dine channel コマンドで受信チャンネルを設定してください！")
+                return
+
     async def on_guild_remove(self, guild):
         with self.session_mng.session_create() as session:
             self.discord_crud.delete_server(session, str(guild.id))
